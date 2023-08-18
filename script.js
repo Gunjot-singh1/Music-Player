41e8986ab5cf7f81a32cca63a7ba53c981b14a78
Gunjot-singh1 <142173954+Gunjot-singh1@users.noreply.github.com>
2023-08-18 17:16:56 +0530

    web dev

---
+++ script.js
@@ -0,0 +1,163 @@
+console.log("Welcome To Spotify");
+
+
+// initialize
+let songIndex=0;
+let audioElement=new Audio('song/1.mp3');
+let masterPlay=document.getElementById('masterPlay');
+let myProgressBar=document.getElementById('myProgressBar');
+let gif=document.getElementById('gif');
+let masterSongName=document.getElementById('masterSongName');
+let songItems=Array.from(document.getElementsByClassName('songItem'));
+
+
+let songs=[
+
+{ songName: "Titli-Satinder Sartaaj", filePath: "song/1.mp3" ,coverPath: "logo.jpg" },
+{ songName: "Tere Vaste-Varun Jain", filePath: "song/2.mp3" ,coverPath: "logo.jpg" },
+{ songName: "Neele Neele Ambar-kalayanji Anandji", filePath: "song/3.mp3" ,coverPath: "logo.jpg" },
+{ songName: "Jhoome Jo Pathaan-Arijit Singh", filePath: "song/4.mp3" ,coverPath: "logo.jpg" },
+{ songName: "Aye Mere wattan-Lata Mangeshkar", filePath: "song/5.mp3" ,coverPath: "logo.jpg" },
+{ songName: "Laembadgini-Diljit Dosanjh", filePath: "song/6.mp3" ,coverPath: "logo.jpg" }
+]
+songItems.forEach((element, i) => {
+   
+    element.getElementsByTagName("img")[0].src=songs[i].coverPath;
+    element.getElementsByClassName("songName")[0].innerText=songs[i].songName;
+
+
+
+
+});
+
+// audioelement play
+
+// handle play/pause
+masterPlay.addEventListener('click', ()=>{
+if(audioElement.paused||audioElement.currentTime<=0)
+
+{
+
+audioElement.play();
+masterPlay.classList.remove('fa-play-circle');
+masterPlay.classList.add('fa-pause-circle');
+gif.style.opacity=1;
+}
+else{
+    audioElement.pause();
+
+    masterPlay.classList.remove('fa-pause-circle');
+    masterPlay.classList.add('fa-play-circle');
+    gif.style.opacity=0;
+
+
+}
+
+
+
+
+})
+
+
+
+
+
+
+// listen to events
+audioElement.addEventListener('timeupdate',()=>{
+console.log('timeupdate');
+// update seekbar
+
+progress=parseInt((audioElement.currentTime/audioElement.duration)* 100);
+myProgressBar.value=progress;
+
+})
+
+myProgressBar.addEventListener('change',()=>{
+audioElement.currentTime=myProgressBar.value*audioElement.duration/100;
+
+
+
+
+
+
+
+})
+
+const makeAllPlays = ()=>{
+    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+        element.classList.remove('fa-pause-circle');
+
+    element.classList.add('fa-play-circle');
+
+
+
+
+})
+}
+
+
+
+
+
+
+Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+
+element.addEventListener('click',(e)=>{
+console.log(e.target);
+makeAllPlays();
+
+songIndex=parseInt(e.target.id);
+e.target.classList.remove('fa-play-circle');
+e.target.classList.add('fa-pause-circle');
+audioElement.src=`song/${songIndex+1}.mp3`;
+masterSongName.innerText=songs[songIndex].songName;
+audioElement.currentTime=0;
+audioElement.play();
+gif.style.opacity=1;
+masterPlay.classList.remove('fa-play-circle');
+masterPlay.classList.add('fa-pause-circle');
+
+
+})
+
+
+})
+
+
+document.getElementById('next').addEventListener('click',()=>{
+    if(songIndex>=5)
+{
+    songIndex=0;
+}
+else{
+    songIndex+=1;
+}
+audioElement.src=`song/${songIndex+1}.mp3`;
+masterSongName.innerText=songs[songIndex].songName;
+audioElement.currentTime=0;
+audioElement.play();
+gif.style.opacity=1;
+masterPlay.classList.remove('fa-play-circle');
+masterPlay.classList.add('fa-pause-circle');
+})
+
+
+
+document.getElementById('previous').addEventListener('click',()=>{
+    if(songIndex<=0)
+{
+    songIndex=0;
+}
+else{
+    songIndex-=1;
+}
+audioElement.src=`song/${songIndex+1}.mp3`;
+masterSongName.innerText=songs[songIndex].songName;
+audioElement.currentTime=0;
+audioElement.play();
+gif.style.opacity=1;
+
+masterPlay.classList.remove('fa-play-circle');
+masterPlay.classList.add('fa-pause-circle');
+})
